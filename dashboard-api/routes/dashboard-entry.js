1586add869a062f353879e6ea30bdcdb194f3892
ja153903 <jaimeabbariao@gmail.com>
2018-10-29 22:34:06 -0400

    Created Model for Dashboard Entry and basic GET and POST

--- dashboard-api/routes/dashboard-entry.js
+++ dashboard-api/routes/dashboard-entry.js
@@ -1,13 +1,29 @@
 const express = require('express')
+const DashboardEntry = require('../model/dashboard-model')
 
 const dashboardEntryRouter = express.Router()
 
-dashboardEntryRouter
-  .get('/1', (req, res) => {
-    res.json([{
-      id: '1',
-      name: 'Jaime'
-    }])
+dashboardEntryRouter.route('/')
+  .get((req, res) => {
+    DashboardEntry.find({}, (err, entries) => {
+      if (err) res.json(err)
+      res.json(entries)
+    })
+  })
+  .post((req, res) => {
+    const entry = new DashboardEntry({
+      entry: req.body.entry
+    })
+    entry.save()
+    res.status(201).send(entry)
+  })
+
+dashboardEntryRouter.route('/:entryId')
+  .get((req, res) => {
+    DashboardEntry.findById(req.params.entryId, (err, entry) => {
+      if (err) res.json(err)
+      res.json(entry)
+    })
   })
 
 module.exports = dashboardEntryRouter
--- dashboard-api/server.js
+++ dashboard-api/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const dashboardEntry = require('./routes/dashboard-entry')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5656
+
+const db_user = process.env.DB_USER
+const db_pass = process.env.DB_PASSWORD
+const db_uri = `mongodb://${db_user}:${db_pass}@ds145573.mlab.com:45573/dashboard-entry-db`
+
+const db = mongoose.connect(db_uri)
 
 app.listen(port, () => {
   console.log(`Port running on localhost:${port}`)
 })
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api/dashboard', dashboardEntry)
